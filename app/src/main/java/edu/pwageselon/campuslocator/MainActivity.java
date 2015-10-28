14f02b9e9bf1787edc603b1f6910191f3828928c
Peter Wages <pwages@elon.edu>
2015-10-28 05:09:28 -0400

    Update layout, add first scanning code

--- app/src/main/java/edu/pwageselon/campuslocator/MainActivity.java
+++ app/src/main/java/edu/pwageselon/campuslocator/MainActivity.java
@@ -3,8 +3,6 @@ package edu.pwageselon.campuslocator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -13,28 +11,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void launchAcademics(View view) {
@@ -53,7 +29,7 @@ public class MainActivity extends Activity {
     }
 
     public void launchActivities(View view) {
-        Intent intent = new Intent(this, ActivitiesActivity.class);
+        Intent intent = new Intent(this, RecreationActivity.class);
         startActivity(intent);
     }
 
